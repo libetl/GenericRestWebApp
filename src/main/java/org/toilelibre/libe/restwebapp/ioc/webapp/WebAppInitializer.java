a98f362bd91c6da5b61c742683486be3ab8175f7
LiBe <libetl@users.noreply.github.com>
2016-01-19 12:21:41 +0100

    Update WebAppInitializer.java

--- src/main/java/org/toilelibre/libe/restwebapp/ioc/webapp/WebAppInitializer.java
+++ src/main/java/org/toilelibre/libe/restwebapp/ioc/webapp/WebAppInitializer.java
@@ -12,22 +12,29 @@ import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-import org.toilelibre.libe.restwebapp.ioc.LuceneSpellCheckerConfig;
 import org.toilelibre.libe.restwebapp.ioc.logs.LogbackConfigListener;
+import org.eclipse.jetty.webapp.AbstractConfiguration;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 /**
  * web.xml in Full JavaConfig
  *
  */
-public class WebAppInitializer implements WebApplicationInitializer {
+public class WebAppInitializer extends AbstractConfiguration implements WebApplicationInitializer {
+	
     private AnnotationConfigWebApplicationContext getContext () {
         final AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext ();
-        context.setConfigLocations (WebAppConfig.class.getName (), LuceneSpellCheckerConfig.class.getName (), WebAppContentNegociationConfig.class.getName (),
+        context.setConfigLocations (WebAppConfig.class.getName (), WebAppContentNegociationConfig.class.getName (),
                 WebMvcConfigurationSupportWithCustomArgumentResolvers.class.getName ());
         return context;
     }
 
     @Override
+	public void configure(WebAppContext context) throws Exception {
+		this.onStartup (context.getServletContext ());
+	}
+
+	@Override
     public void onStartup (final ServletContext servletContext) throws ServletException {
         final WebApplicationContext context = this.getContext ();
         servletContext.addListener (new LogbackConfigListener ());
@@ -42,5 +49,4 @@ public class WebAppInitializer implements WebApplicationInitializer {
         dispatcher.setInitParameter ("throwExceptionIfNoHandlerFound", "true");
         dispatcher.addMapping ("/api/*");
     }
-
 }
